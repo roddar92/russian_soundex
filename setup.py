d7c6f3e697087140d4aa5fbe9a5e50e0e1b3c024
Dashka Rodionova <rodionowadarja@yandex.ru>
2018-12-24 18:07:26 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from os.path import join, dirname
 setup(
     name='ru_soundex',
     version='1.0',
+    url='https://github.com/roddar92/ru_soundex',
+    author='Daria Rodionoa',
+    author_email='drodionova86@gmail.com',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(), install_requires=['pymorphy2', 'editdistance']
 )
